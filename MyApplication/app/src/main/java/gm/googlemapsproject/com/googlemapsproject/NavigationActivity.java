5fc676386114e45c5141badfff54bc2e597ab372
sungly <sungly.77@gmail.com>
2016-09-02 19:10:46 -0400

    google maps now works in navigation drawer

--- MyApplication/app/src/main/java/gm/googlemapsproject/com/googlemapsproject/NavigationActivity.java
+++ MyApplication/app/src/main/java/gm/googlemapsproject/com/googlemapsproject/NavigationActivity.java
@@ -1,9 +1,12 @@
 package gm.googlemapsproject.com.googlemapsproject;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +17,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class NavigationActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Circle circle;
+    private double currentLat;
+    private double currentLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +62,53 @@ public class NavigationActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //googlemaps
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.content_map);
+            mapFragment.getMapAsync(this);
+
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Location currently set in Newmarket, ON
+        LatLng queens = new LatLng(44.053607, -79.458481);
+        //LatLng queens = new LatLng(currentLat, currentLong);
+        //adds the market description
+        mMap.addMarker(new MarkerOptions().position(queens).title("Marker at Queen's"));
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //when clicked move camera to user's current location
+        mMap.setMyLocationEnabled(true);
+
+        //adds the specify location and zoom in by 17
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(queens, 17));
+
+        //adds a circle radius around the specifiy location
+        circle = mMap.addCircle(new CircleOptions()
+                .center(queens)
+                .radius(1000)
+                .strokeWidth(10)
+                .strokeColor(Color.GREEN)
+                .fillColor(0x5500ff00)
+                .clickable(true));
+
+    }//end on mapready
 
     @Override
     public void onBackPressed() {
@@ -101,4 +166,5 @@ public class NavigationActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
