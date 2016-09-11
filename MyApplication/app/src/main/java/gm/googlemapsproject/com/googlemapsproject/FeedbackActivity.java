ad4c8c7c8670b3defb30e337bff5631c9eccfa8a
sungly <sungly.77@gmail.com>
2016-09-11 18:31:38 -0400

    started the feedback activity

---
+++ MyApplication/app/src/main/java/gm/googlemapsproject/com/googlemapsproject/FeedbackActivity.java
@@ -0,0 +1,40 @@
+package gm.googlemapsproject.com.googlemapsproject;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(Color.parseColor("#1ca7f7"));
+
+
+        getSupportActionBar().setTitle("FeedBack");
+
+        //displays the back  button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }//end onCreate
+
+}
