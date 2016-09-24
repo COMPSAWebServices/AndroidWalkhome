353b6dc475e7d992b9d9acfd15afec8a24cd50c1
sungly <sungly.77@gmail.com>
2016-09-24 11:16:02 -0400

    added addWalkhomeApi() to action floating button

--- MyApplication/app/src/main/java/gm/googlemapsproject/com/googlemapsproject/NavigationActivity.java
+++ MyApplication/app/src/main/java/gm/googlemapsproject/com/googlemapsproject/NavigationActivity.java
@@ -157,11 +157,14 @@ public class NavigationActivity extends AppCompatActivity
 
         //floating button -> ***Change this later
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                addWalkToApi();
+
             }
         });
 
